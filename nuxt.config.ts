e4e5982a20800005d8334d5c20c51945571362db
Andrew Raub <raublekick@gmail.com>
2025-10-29 20:24:28 -0700

    More env cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,11 @@ export default defineNuxtConfig({
   ],
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   runtimeConfig: {
-    gdriveApiKey: process.env.NUXT_GDRIVE_API_KEY || '',
-    sheetId: process.env.NUXT_SHEET_ID || '',
-    tabName: process.env.NUXT_TAB_NAME || '',
+    gdriveApiKey: '',
+    sheetId: '',
+    tabName: '',
     public: {
-      mapboxKey: process.env.NUXT_PUBLIC_MAPBOX_KEY || ''
+      mapboxKey: ''
     }
   }
 })
